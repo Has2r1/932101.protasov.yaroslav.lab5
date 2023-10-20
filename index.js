710fdb4517eebc3ce1d7b89a97b4d1b61c9db63b
Has2r1 <128485726+Has2r1@users.noreply.github.com>
2023-10-20 19:56:54 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function button_1() {
+	document.getElementById("popup_block_1").style.display = "block";
+	document.getElementById("main_class").style.opacity = "0.5";
+}
+function button_2() {
+	document.getElementById("popup_block_2").style.display = "block";
+	document.getElementById("main_class").style.opacity = "0.5";
+}
+function close_1() {
+	document.getElementById("popup_block_1").style.display = "none";
+	document.getElementById("main_class").style.opacity = "1";
+}
+function close_2() {
+	document.getElementById("popup_block_2").style.display = "none";
+	document.getElementById("main_class").style.opacity = "1";
+}
