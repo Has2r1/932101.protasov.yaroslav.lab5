906056bff85f1a133cb4ce940962940e29d1f962
Has2r1 <128485726+Has2r1@users.noreply.github.com>
2023-10-21 19:54:09 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -6,11 +6,19 @@ function button_2() {
 	document.getElementById("popup_block_2").style.display = "block";
 	document.getElementById("main_class").style.opacity = "0.5";
 }
+function button_3() {
+	document.getElementById("popup_block_3").style.display = "block";
+	document.getElementById("main_class").style.opacity = "0.5";
+}
 function close_1() {
 	document.getElementById("popup_block_1").style.display = "none";
 	document.getElementById("main_class").style.opacity = "1";
 }
 function close_2() {
 	document.getElementById("popup_block_2").style.display = "none";
+	document.getElementById("main_class").style.opacity = "1";
+}
+function close_3() {
+	document.getElementById("popup_block_3").style.display = "none";
 	document.getElementById("main_class").style.opacity = "1";
 }
